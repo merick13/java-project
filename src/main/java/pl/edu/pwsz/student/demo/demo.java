04ffdd6ef2a8bcd6d6d473e9a88bb2351723a76d
Marek Kamiński <alekkam@wp.pl>
2017-05-30 19:16:35 +0200

    initial commit

---
+++ src/main/java/pl/edu/pwsz/student/demo/demo.java
@@ -0,0 +1,11 @@
+package pl.edu.pwsz.student.demo;
+
+/**
+ * Created by Student on 2017-05-30.
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("Michał");
+    }
+}
