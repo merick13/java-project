e2039a9845a9258a8376d33e60a4a7bebb024ec4
Marek Kamiński <alekkam@wp.pl>
2017-05-30 19:36:17 +0200

    dodano klase Student

---
+++ src/main/java/pl/edu/pwsz/student/demo/Student.java
@@ -0,0 +1,37 @@
+package pl.edu.pwsz.student.demo;
+
+/**
+ * Created by Student on 2017-05-30.
+ */
+public class Student {
+
+    private int yearofBirth;
+
+    private String name;
+
+    private String lastname;
+
+    public int getYearofBirth() {
+        return yearofBirth;
+    }
+
+    public void setYearofBirth(int yearofBirth) {
+        this.yearofBirth = yearofBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
